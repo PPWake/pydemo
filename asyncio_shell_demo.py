802df3aaee4924184a8046c4249393aea8f9af1c
tanpengyao <tanzz1024@gmail.com>
2020-04-01 19:44:39 +0800

    fix_bug

--- asyncio_shell_demo.py
+++ asyncio_shell_demo.py
@@ -1,36 +1,45 @@
-import asyncio.subprocess
 import sys
+import asyncio
 
-async def execute_command(cmd):
+async def execute_command(cmd:str, whichsh="/bin/bash", raiseException=False):
     if not cmd:
       return 1, "cmd is none!"
-      
+
     try:
       # create is a conroutes
-      create = asyncio.create_subprocess_shell(cmd, stderr=asyncio.subprocess.PIPE, 
-                                                stdout=asyncio.subprocess.PIPE)
-      # create a process
-      proc = await create
+      create_coroutine = asyncio.create_subprocess_exec(whichsh, "-c", cmd, stderr=asyncio.subprocess.PIPE, 
+                                              stdout=asyncio.subprocess.PIPE)
 
-      # avoid proc.wait block by the big output
-      (stdout_data, stderr_data)=  await proc.communicate()
-      output = stdout_data if stdout_data else stderr_data
-      
-      # 0 is success
-      status = 1
+      # create a process
+      proc = await create_coroutine
+      (stdout, stderr)=  await proc.communicate()
+      if stdout:
+        stdout = stdout.decode()
+        stdout = stdout.strip("\n")
+
+      if stderr:
+        stderr = stderr.decode()
+        stderr = stderr.strip("\n")
+
       # wait for terminal and get status
       status = await proc.wait()
     except Exception as e:
-      print("Get exception %s" % (str(e)))
-      return 1, str(e)
-    return status, output
+      if raiseException:
+        raise e
+      return 1, None, str(e)
+    return status, stdout, stderr
 
-if sys.platform == "win32":
-    loop = asyncio.ProactorEventLoop()
-    asyncio.set_event_loop(loop)
-else:
-    loop = asyncio.get_event_loop()
 
-status, output = loop.run_until_complete(execute_command("ls -l"))
-print("show value status: %s output: %s" % (str(status), output.decode()))
-loop.close()
+def test_command():
+  if sys.platform == "win32":
+      loop = asyncio.ProactorEventLoop()
+      asyncio.set_event_loop(loop)
+  else:
+      loop = asyncio.get_event_loop()
+
+  status, stdout, stderr = loop.run_until_complete(execute_command("ls -l"))
+  print("show value status: %s \nstdout: %s \nstderr:%s"  % (str(status), stdout, stderr))
+  loop.close()
+
+if __name__ == "__main__":
+    test_command()
